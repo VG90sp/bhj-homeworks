e8610d9361a6c9d8228ca0be714766b6fe66bc05
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-12-02 19:22:52 +0300

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,13 @@
+let menuLink = Array.from(document.getElementsByClassName('menu__link'));
+
+for (let link of menuLink) {
+    link.onclick = function() {
+      if (link.parentElement.querySelector(".menu_sub")) {
+          if (link.closest(".menu_main").querySelector(".menu_active")) {
+                link.closest(".menu_main").querySelector(".menu_active").classList.remove("menu_active");
+            }
+            link.parentElement.querySelector(".menu_sub").classList.add("menu_active");
+            return false;
+        }
+    }
+}
