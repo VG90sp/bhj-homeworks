ca01579f7ffd47bf80796ac5caaec693d49c05db
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-12-06 21:03:24 +0300

    fix bug

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,18 +1,19 @@
 const modalMain = document.getElementById('modal_main');
 const modalSuccess = document.getElementById('modal_success');
 const modalClose = document.getElementsByClassName('modal__close');
+const showSuccess = document.getElementsByClassName('show-success')[0];
 
 modalMain.classList.add('modal_active');
 
-modalClose.item(0).onclick = function () {
-    modalMain.classList.remove('modal_active');
-}
-
-modalClose.item(1).onclick = function () {
-    modalMain.classList.remove('modal_active');
+showSuccess.addEventListener('click', () => {
     modalSuccess.classList.add('modal_active');
-}
+});
 
-modalClose.item(2).onclick = function () {
-    modalSuccess.classList.remove('modal_active');
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose.item(i).addEventListener('click', function() {
+        const modal = this.closest('.modal');
+        if (modal) {
+            modal.classList.remove('modal_active');
+        }
+    });
 }
