b68525461e438369d03f19d33b691b3709898cfe
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-12-06 21:58:35 +0300

    fix bug

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,26 +1,17 @@
 const prev = document.getElementsByClassName('slider__arrow_prev')[0];
 const next = document.getElementsByClassName('slider__arrow_next')[0];
-const slides = document.getElementsByClassName('slider__item');
-let activeSlideIndex = 0;
+const slides = Array.from(document.getElementsByClassName('slider__item'));
 
 prev.onclick = () => {
-  const oldActiveSlideIndex = activeSlideIndex;
-  activeSlideIndex = activeSlideIndex - 1;
-
-  if (activeSlideIndex < 0) {
-    activeSlideIndex = slides.length - 1;
-  }
-
-  slides[activeSlideIndex].classList.add('slider__item_active');
-  slides[oldActiveSlideIndex].classList.remove('slider__item_active');
+  const activeSlide = slides.findIndex((slide) => slide.classList.contains('slider__item_active'));
+  const nextSlideIndex = activeSlide < 0 ? slides.length - 1 : activeSlide - 1;
+  slides[activeSlide].classList.remove('slider__item_active');
+  slides[newSlideIndex].classList.add('slider__item_active');
 };
 
 next.onclick = () => {
-    const oldActiveSlideIndex = activeSlideIndex;
-    activeSlideIndex = activeSlideIndex + 1;
-    if (slides.length <= activeSlideIndex) {
-        activeSlideIndex = 0 ;
-    }
-    slides[oldActiveSlideIndex].classList.remove('slider__item_active');
-    slides[activeSlideIndex].classList.add('slider__item_active');
+  const activeSlide = slides.findIndex((slide) => slide.classList.contains('slider__item_active'));
+  const newSlideIndex = activeSlide >= slides.length - 1 ? 0 : activeSlide + 1;
+  slides[activeSlide].classList.remove('slider__item_active');
+  slides[newSlideIndex].classList.add('slider__item_active');
 };
