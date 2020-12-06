c65475a343bc4c1c8897f1b93788ef33e90ecc03
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-12-06 21:15:29 +0300

    fix bug

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,8 +5,9 @@ for (let link of menuLink) {
       if (link.parentElement.querySelector(".menu_sub")) {
           if (link.closest(".menu_main").querySelector(".menu_active")) {
                 link.closest(".menu_main").querySelector(".menu_active").classList.remove("menu_active");
+            } else {
+                link.parentElement.querySelector(".menu_sub").classList.add("menu_active");
             }
-            link.parentElement.querySelector(".menu_sub").classList.add("menu_active");
             return false;
         }
     }
