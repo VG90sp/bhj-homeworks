fb3d9a810bf82a9b79fcdd6e640010edf00ff6c3
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2020-12-16 15:53:37 +0300

    first commit reveal

---
+++ dom/reveal/task.js
@@ -0,0 +1,13 @@
+const reveal = document.querySelectorAll('.reveal');
+
+for (let i = 0; i < reveal.length; i++) {
+
+	document.addEventListener('scroll', () => {
+
+        if(reveal[i].getBoundingClientRect().top < window.innerHeight) {
+            reveal[i].classList.add('reveal_active');
+        } else {
+			reveal[i].classList.remove('reveal_active');
+		}    
+  });
+}
